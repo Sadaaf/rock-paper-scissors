37aaae3f6f7fd73053bcd83729f7fec45c6b57e2
Sadaf Al Arabi <35129264+Sadaaf@users.noreply.github.com>
2024-09-15 13:07:43 -0400

    feat: add ui

--- script.js
+++ script.js
@@ -15,41 +15,77 @@ function getHumanChoice() {
   return choice;
 }
 
-function playRound() {
+function playRound(humanChoice) {
   computerChoice = getComputerChoice();
-  humanChoice = getHumanChoice();
+  const result = document.querySelector(".result");
+  showResult = (text) => {
+    result.innerText = text;
+  };
   if (computerChoice === humanChoice) {
-    alert(`It's a TIE you both choose ${computerChoice}`);
+    showResult(`It's a TIE you both choose ${computerChoice}`);
   } else if (computerChoice === "rock" && humanChoice === "scissors") {
-    alert(`You lose, rock beats scissors`);
+    showResult(`You lose, rock beats scissors`);
     computerScore++;
   } else if (computerChoice === "paper" && humanChoice === "rock") {
-    alert(`You lose, paper beats rock`);
+    showResult(`You lose, paper beats rock`);
     computerScore++;
   } else if (computerChoice === "scissors" && humanChoice === "paper") {
-    alert(`You lose, scissors beats paper`);
+    showResult(`You lose, scissors beats paper`);
     computerScore++;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    alert(`You win, rock beats scissors`);
+    showResult(`You win, rock beats scissors`);
     humanScore++;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    alert(`You win, paper beats rock`);
+    showResult(`You win, paper beats rock`);
     humanScore++;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    alert(`You win, scissors beats paper`);
+    showResult(`You win, scissors beats paper`);
     humanScore++;
   }
 }
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
+  score = document.querySelector(".score");
+  showScore = (text) => {
+    score.innerText = text;
+  };
+
+  showScore(`Player: ${humanScore}\nComputer: ${computerScore}`);
+
+  if (humanScore === 5) {
+    showScore(`You WON!!! Congratulations\nScore: ${humanScore}`);
+    buttons.forEach((button) => {
+      buttons.forEach((button) => {
+        button.disabled = true;
+      });
+    });
+  } else if (computerScore == 5) {
+    showScore(`Sorry, You LOST!!!!\nScore: ${humanScore}`);
+    buttons.forEach((button) => {
+      buttons.forEach((button) => {
+        button.disabled = true;
+      });
+    });
   }
-  if (humanScore > computerScore) {
-    alert(`You WON!!! Congratulations\nScore: ${humanScore}`);
-  } else if (humanScore < computerScore) {
-    alert(`Sorry, You LOST!!!!\nScore: ${humanScore}`);
-  }
+  !humanScore &&
+    !computerScore &&
+    buttons.forEach((button) => {
+      buttons.forEach((button) => {
+        button.disabled = false;
+      });
+    });
 }
 
-playGame();
+//////////////
+// Handlers //
+/////////////
+
+function handleClick(e) {
+  playRound(e.target.value);
+  playGame();
+}
+
+const buttons = document.querySelectorAll(".selection button");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
